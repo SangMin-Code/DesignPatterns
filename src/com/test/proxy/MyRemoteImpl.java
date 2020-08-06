97e27b91d1d510c590ddb0bf84a2439174bba5a6
SangMin-Code <ragdexplorer@gmail.com>
2020-08-06 22:00:23 +0900

    20200806 rmi for proxy

---
+++ src/com/test/proxy/MyRemoteImpl.java
@@ -0,0 +1,29 @@
+package com.test.proxy;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote{
+	
+	public MyRemoteImpl() throws RemoteException{}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String sayHello() throws RemoteException {
+		return "Server says, 'Hey'";
+	}
+	
+	public static void main(String[] args) {
+		try {
+			MyRemote service = new MyRemoteImpl();
+			Naming.rebind("RemoteHello", service);
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+
+}
